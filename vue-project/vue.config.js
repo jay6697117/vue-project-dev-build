e05d5d48ca9e901add04f153f7dc1b71c36e33d6
zhangjinhui <871506263@qq.com>
2021-07-01 19:07:21 +0800

    提交代码于: 2021-07-01 19:07:21

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -2,7 +2,7 @@ const publicPath = process.env.NODE_ENV === 'production' ? '/subpath/' : '/'
 
 module.exports = {
   publicPath,
-  outputDir: 'output',
+  outputDir: 'dist',
   devServer: {
     open: true, // 是否自动打开浏览器页面
     host: '0.0.0.0', // 指定使用一个 host。默认是 localhost
