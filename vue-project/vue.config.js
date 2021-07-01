bcc3ddb5dc6580c364f0ab5e92bfb2eb2d9f38a0
steve zhang <871506263@qq.com>
2021-07-02 00:45:45 +0800

    提交代码于: 2021-07-02 00:45:45

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -1,8 +1,22 @@
+// const path = require('path')
+// const webpack = require('webpack')
+const merge = require('webpack-merge')
 const publicPath = process.env.NODE_ENV === 'production' ? '/subpath/' : '/'
 
 module.exports = {
   publicPath,
   outputDir: 'dist',
+  productionSourceMap: true,
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .tap(options =>
+        merge(options, {
+          limit: 5120
+        })
+      )
+  },
   devServer: {
     open: true, // 是否自动打开浏览器页面
     host: '0.0.0.0', // 指定使用一个 host。默认是 localhost
